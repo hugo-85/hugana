5d7070331c2d56d721a85383208589dea6e14069
hugo-85 <hugomargiotta@gmail.com>
2025-09-18 20:31:58 -0300

    remove unused state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import Hero from "./components/hero/Hero";
 import Skills from "./components/skills/Skills";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <main>
